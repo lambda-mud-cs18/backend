10882020f97cf7d1df66bfbc1398f3ba4d474d3f
Brian Illes <brianrilles@gmail.com>
2019-07-29 15:23:05 -0700

    Tables set up

--- mud/admin.py
+++ mud/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Player, PlayerInventory, Item, Team, Map, Room
 # Register your models here.
+admin.site.register(Player)
+admin.site.register(PlayerInventory)
+admin.site.register(Item)
+admin.site.register(Team)
+admin.site.register(Map)
+admin.site.register(Room)
--- mud/models.py
+++ mud/models.py
@@ -4,12 +4,13 @@ from uuid import uuid4
 # Create your models here.
 
 
-class User(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    username = models.CharField(max_length=None)
-    name = models.CharField(max_length=None)
-    password = models.CharField(max_length=None)
-    team_id = model.IntegerField()
+class Player(models.Model):
+    # id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    id = models.AutoField(primary_key=True)
+    playername = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    team_id = models.IntegerField()
     # current room details
     current_room = models.IntegerField()
     cooldown = models.FloatField()
@@ -17,33 +18,78 @@ class User(models.Model):
     strength = models.IntegerField()
     speed = models.IntegerField()
     gold = models.IntegerField()
-    inventory = models.ArrayField(models.CharField(max_length=None), size=None)
-    status = models.ArrayField(models.CharField(max_length=None), size=None)
-    errors = models.ArrayField(models.CharField(max_length=None), size=None)
-    messages = models.ArrayField(models.CharField(max_length=None), size=None)
+    inventory = models.CharField(max_length=1000)
+    status = models.CharField(max_length=1000)
+    errors = models.CharField(max_length=1000)
+    messages = models.CharField(max_length=1000)
+    token = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.name
+
+    def get_current_room(self):
+        return self.current_room
 
 
-class Team(model.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    name = models.CharField(max_length=None)
+class PlayerInventory(models.Model):
+    # id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    id = models.AutoField(primary_key=True)
+
+    player_id = models.IntegerField()
+    item_id = models.IntegerField()
+    quantity = models.IntegerField()
+
+
+class Item(models.Model):
+    # id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    weight = models.IntegerField()
+    itemtype = models.CharField(max_length=200)
+    level = models.IntegerField()
+    exp = models.IntegerField()
+    attributes = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Team(models.Model):
+    # id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    id = models.AutoField(primary_key=True)
+
+    name = models.CharField(max_length=200)
     map_id = models.IntegerField()
+
+    def __str__(self):
+        return self.name
 
 
 class Map(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    map_id = models.IntegerField()
+    id = models.AutoField(primary_key=True)
+    # id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    # map_id = models.IntegerField()
+
+    # def __str__(self):
+    #     return self.map_id
+    pass
 
 
 class Room(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    # id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    id = models.AutoField(primary_key=True)
     map_id = models.IntegerField()
     room_id = models.IntegerField()
-    title = models.CharField(max_length=None)
-    description = models.CharField(max_length=None)
-    coordinates = models.CharField(max_length=None)
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    coordinates = models.CharField(max_length=200)
     elevation = models.IntegerField()
-    terrain = models.CharField(max_length=None)
-    north = models.CharField(max_length=None)
-    south = models.CharField(max_length=None)
-    east = models.CharField(max_length=None)
-    west = models.CharField(max_length=None)
+    terrain = models.CharField(max_length=200)
+    north = models.CharField(max_length=200)
+    south = models.CharField(max_length=200)
+    east = models.CharField(max_length=200)
+    west = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.room_id
