e5e2527c6b4fdbdf472ad3a3d5b7c4651919bd25
brooksvinyard <8202653+brooksvinyard@users.noreply.github.com>
2019-07-29 17:10:32 -0700

    Added player functions

--- mud/models.py
+++ mud/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from uuid import uuid4
+import requests
+
+url = "https://lambda-treasure-hunt.herokuapp.com"
+# token = 
 
 # Create your models here.
 
@@ -29,6 +33,54 @@ class Player(models.Model):
 
     def get_current_room(self):
         return self.current_room
+    def travel(self, direction, next_room_id=None):
+        # Function for the player to travel to the next room
+        # Needs to be done after the cooldown
+        pass
+    def take(self):
+        # Pickup the item in the current room
+        # add the item to the player inventory
+        # Needs to be done after the cooldown
+
+        # If there arre multiple items in the room, pick them all up
+        # for item in  res.items:
+            # API call to TAKE
+            # https://lambda-treasure-hunt.herokuapp.com/api/adv/take/
+            # {
+            #     "name":"{item}"
+            # }
+        pass
+    def get_status(self):
+        # https://lambda-treasure-hunt.herokuapp.com/api/adv/status/
+        post_data = {}
+        headers = {'content-type': 'application/json', 'Authorization': 'Token ' + self.token}
+        r = requests.post(url=url + "/api/adv/status/", json=post_data, headers=headers)
+        data = r.json()
+        print(data)
+        if data.get('errors') is []:
+            # To test out the function...
+            # python3 manage.py shell
+            # from mud.models import Player
+            # p = Player.objects.get(name = 'player85')
+            # p.get_status()
+
+            # Update the player information in the DB
+            Player.objects.filter(name = data.get('name')).update(name = data.get('name'),
+                cooldown = data.get('cooldown'),
+                encumbrance = data.get('encumbrance'),
+                strength = data.get('strength'),
+                speed = data.get('speed'),
+                gold = data.get('gold'),
+                inventory = data.get('inventory'),
+                status = data.get('status'),
+                errors = data.get('errors'),
+                messages = data.get('messages'))
+            
+            # return data
+        else:
+            # There was an error
+            Player.objects.filter(name = self.name).update(cooldown = data.get('cooldown'))
+            print(data.get('cooldown'))
 
 
 class PlayerInventory(models.Model):
@@ -93,3 +145,16 @@ class Room(models.Model):
 
     def __str__(self):
         return self.room_id
+    
+    def getExits(self):
+        exits = []
+        if self.north is not None:
+            exits.append("n")
+        if self.south is not None:
+            exits.append("s")
+        if self.east is not None:
+            exits.append("e")
+        if self.west is not None:
+            exits.append("w")
+        return exits
+
